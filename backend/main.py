daa8e9efaf523adc9ce70aed5c4613976bf6c0fb
Joe Stanley <33275230+engineerjoe440@users.noreply.github.com>
2024-01-24 16:50:29 -0800

    Started laying out repo layout

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+################################################################################
+"""
+Audi-Hose
+FOSS Speakpipe alternative built to connect audiences to the creators they love.
+
+License: AGPL-3.0
+Author: Joe Stanley
+"""
+################################################################################
